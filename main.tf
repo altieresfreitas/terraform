741944272b90de415b905c0fedddbb01fb8334a7
Altieres Freitas <altieressfreitas@gmail.com>
2019-09-12 01:28:07 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "openstack_compute_instance_v2" "basic" {
+  name            = "basic"
+  image_id        = "ad091b52-742f-469e-8f3c-fd81cadf0743"
+  flavor_id       = "3"
+  key_pair        = "my_key_pair_name"
+  security_groups = ["default"]
+
+  metadata = {
+    this = "that"
+  }
+
+  network {
+    name = "my_network"
+  }
+}
